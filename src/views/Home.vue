0b261379962dabaaf2542702ffbb43e10afc86db
ll9 <llwierer@web.de>
2019-09-01 18:45:49 +0200

    menu component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Menu />
     <HelloWorld/>
   </div>
 </template>
@@ -7,10 +8,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Menu from './Menu.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Menu,
   },
 })
 export default class Home extends Vue {}
---
+++ src/views/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pure-menu pure-menu-horizontal" id="status-bar">
+    <ul class="pure-menu-list">
+      <li class="pure-menu-item">
+        <router-link to="/suche" class="pure-menu-link">Suche</router-link>
+      </li>
+      <li class="pure-menu-item">
+        <router-link to="/liste" class="pure-menu-link">Liste</router-link>
+      </li>
+      <li class="pure-menu-item">
+        <router-link to="/archiv" class="pure-menu-link">Archiv</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#status-bar {
+  position: absolute;
+  top: 0;
+}
+</style>
