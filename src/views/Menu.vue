40544a728a2414e1929923afd7082871b204aab3
ll9 <llwierer@web.de>
2019-09-01 19:04:32 +0200

    active highlighting

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,13 +2,13 @@
   <div class="pure-menu pure-menu-horizontal" id="status-bar">
     <ul class="pure-menu-list">
       <li class="pure-menu-item">
-        <router-link to="/suche" class="pure-menu-link">Suche</router-link>
+        <router-link to="/suche" class="pure-menu-link" :class="{ active: $route.path=='/suche'}">Suche</router-link>
       </li>
       <li class="pure-menu-item">
-        <router-link to="/liste" class="pure-menu-link">Liste</router-link>
+        <router-link to="/liste" class="pure-menu-link" :class="{ active: $route.path=='/liste'}">Liste</router-link>
       </li>
       <li class="pure-menu-item">
-        <router-link to="/archiv" class="pure-menu-link">Archiv</router-link>
+        <router-link to="/archiv" class="pure-menu-link" :class="{ active: $route.path=='/archiv'}">Archiv</router-link>
       </li>
     </ul>
   </div>
