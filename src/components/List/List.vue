2b14ea9199329a1d857a251f4ab307323034f321
ll9 <llwierer@web.de>
2019-09-14 22:54:47 +0200

    CRUD Users

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -70,6 +70,5 @@ export default Vue.extend({
 .grid-container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  border: 1px solid black;
 }
 </style>
--- src/components/List/UsersPanel.vue
+++ src/components/List/UsersPanel.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="grid-container">
+  <div class="user-grid-container">
     <div v-for="user in users" :key="user.id">
-      <p class="inline mr-2">{{user.name}}</p>
-      <button @click="removeUser(user.id)">-</button>
+      <div class="card-header">
+        <p class="inline mr-2">{{user.name}}</p>
+        <button @click="removeUser(user.id)" class="pure-button">-</button>
+      </div>
       <draggable
         :list="user.games"
         class="list-group user-items"
@@ -20,8 +22,8 @@
         />
       </draggable>
     </div>
-    <div v-if="users.length < 4">
-      <button @click="addUser" class="inline">+</button>
+    <div v-if="users.length < 4" class="card-header">
+      <button @click="addUser" class="inline pure-button">+</button>
     </div>
   </div>
 </template>
@@ -44,16 +46,25 @@ export default Vue.extend({
       ],
     };
   },
+  mounted() {
+    this.loadUsers();
+  },
   methods: {
-    addUser() {
+    async addUser() {
       const user = prompt('Username');
       if (user) {
-        this.users.push({ id: this.users.length + 1, name: user, games: [] });
+        const res = await internalContext.users.add(user);
+        this.users.push({ id: res.data.id, name: res.data.name, games: [] });
       }
     },
-    removeUser(id: any) {
+    async loadUsers() {
+      const res = await internalContext.users.list();
+      this.users = res.data.map((d: any) => ({ ...d, games: [] }));
+    },
+    async removeUser(id: any) {
       const index = this.users.findIndex((u) => u.id === id);
       if (index !== -1) {
+        await internalContext.users.remove(id);
         this.users.splice(index, 1);
       }
     },
@@ -80,17 +91,35 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.grid-container {
+.user-grid-container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 
   > div {
     border: 1px solid black;
+
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+
+    &:hover {
+      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
   }
+}
+
+.card-header {
+  padding: 10px 0;
+  border-bottom: 1px solid black;
 }
 
 .user-items {
   padding: 10px;
   min-height: 300px;
+}
+
+.list-group{
+  border: 3px dashed black;
+  margin: 20px;
 }
 </style>
