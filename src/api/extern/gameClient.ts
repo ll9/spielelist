9ee7441921eb45f16c2cbcc3a04d7ad484919cbb
ll9 <llwierer@web.de>
2020-08-26 10:17:03 +0200

    removed popularity from seach

--- src/api/extern/gameClient.ts
+++ src/api/extern/gameClient.ts
@@ -1,7 +1,7 @@
-import client from "./baseClient";
-import { AxiosResponse } from "axios";
+import client from "./baseClient"
+import { AxiosResponse } from "axios"
 
-const resource = "games";
+const resource = "games"
 
 export default {
   async search(val: string): Promise<AxiosResponse<any>> {
@@ -10,26 +10,26 @@ export default {
       `fields name, popularity, cover.url, url, first_release_date;
       search "${val}";`,
       { headers: { "content-type": "text/plain" } }
-    );
+    )
   },
   async recentReleases(): Promise<AxiosResponse<any>> {
-    const date = new Date(new Date().setMonth(new Date().getMonth() + 0));
-    const timeStamp = Math.floor(date.getTime() / 1000);
+    const date = new Date(new Date().setMonth(new Date().getMonth() + 0))
+    const timeStamp = Math.floor(date.getTime() / 1000)
 
     return await client.post(
       `/${resource}`,
       `
       fields name, popularity, cover.url, url, first_release_date;
-      where platforms = 130 & first_release_date > 1538129354 & first_release_date < ${timeStamp} & cover.url != null & popularity > 4;
+      where platforms = 130 & first_release_date > 1538129354 & first_release_date < ${timeStamp} & cover.url != null;
       sort first_release_date desc;
       limit 25;
       `,
       { headers: { "content-type": "text/plain" } }
-    );
+    )
   },
   async listByIds(ids: number[]): Promise<object[]> {
     if (ids.length === 0) {
-      return [];
+      return []
     }
 
     let res = await client.post(
@@ -38,8 +38,8 @@ export default {
       where id = (${ids.join(",")});
       limit 50;`,
       { headers: { "content-type": "text/plain" } }
-    );
+    )
 
-    return res.data;
-  },
-};
+    return res.data
+  }
+}
